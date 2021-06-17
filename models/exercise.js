795dcdf224e93c07f09ccab7e92096ac252ec732
rsheppard83 <rsheppard83@gmail.com>
2021-06-17 22:28:27 +0100

    deleted tojson function

--- models/exercise.js
+++ models/exercise.js
@@ -17,18 +17,6 @@ const exerciseSchema = new mongoose.Schema({
     }
 })
 
-// hide version, exercise id and user id from being sent back to client
-exerciseSchema.methods.toJSON = function () {
-    const exercise = this
-    const exerciseObject = exercise.toObject()
-
-    delete exerciseObject._id
-    delete exerciseObject.__v
-    delete exerciseObject.owner
-
-    return exerciseObject
-}
-
 const Exercise = mongoose.model('Exercise', exerciseSchema)
 
 module.exports = Exercise
