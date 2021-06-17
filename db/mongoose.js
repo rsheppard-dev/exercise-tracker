adc25938f13a3a4d21ffe58e2de0342cf157e115
rsheppard83 <rsheppard83@gmail.com>
2021-06-17 18:30:14 +0100

    changed db settings

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,8 +2,5 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    autoIndex: true
+    useUnifiedTopology: true
 })
